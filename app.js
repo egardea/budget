f48c8b160880075f3c8fcba3941d4d790d7007f7
Erik Gardea <erik1gardea@gmail.com>
2019-10-06 17:43:10 -0500

    created an object ot store the input values

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ const budgetController = (() => {
 //UI controller
 const UIController = (() => {
 
+    const DOMStrings = {
+        inputType: '.add__type',
+        description: '.add__description',
+        value: '.add__value',
+    }
+
     return {
         getInput: () => {
-            let type = document.querySelector('.add__type').value; //will be either INC or EXP
-            let description = document.querySelector('.add__description').value; //description
-            let value = document.querySelector('.add__value').value; //value amount
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, //will be either INC or EXP
+                description: document.querySelector(DOMStrings.description).value, //description
+                value: document.querySelector(DOMStrings.value).value, //value amount
+            }
         }
     }
 
@@ -23,9 +31,28 @@ const controller = ((budgetCtrl, UICtrl) => {
 
     const ctrlAddItem = () => {
         //get the input data
-        
-        //add the item to the budget controller
+        let input = UICtrl.getInput();
+        console.log(input);
 
+        //add the item to the budget controller
+        const listItem = `
+            <div class="item clearfix" id="${input.type === 'exp' ? 'expense' : 'income'}-0">
+                <div class="item__description">${input.description}</div>
+                <div class="right clearfix">
+                    <div class="item__value">${input.type === 'exp' ? '-' : '+'} ${parseInt(input.value)}</div>
+                    ${input.type === 'exp' ? `<div class="item__percentage">21%</div>` : ''}
+                    <div class="item__delete">
+                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                    </div>
+                </div>
+            </div>  
+        `;
+
+        if(input.type === 'exp') {
+            document.querySelector('.expenses__list').insertAdjacentHTML('beforeend', listItem);
+        } else if(input.type === 'inc') {
+            document.querySelector('.income__list').insertAdjacentHTML('beforeend', listItem);
+        }
         //add the item to the UI
 
         //calculate the budget
