6c08a507a7d6fd6ef0eb36311e90cfc667a1539f
Erik Gardea <erik1gardea@gmail.com>
2019-10-06 19:02:22 -0500

    expense and income classes created to save each item and calculate the totals

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 //Budget controller
 const budgetController = (() => {
 
+    class Expense {
+        constructor(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+    };
 
+    class Income {
+        constructor(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+    };
+
+    const data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+    };
 
 })();
 
@@ -12,6 +37,9 @@ const UIController = (() => {
         inputType: '.add__type',
         description: '.add__description',
         value: '.add__value',
+        inputBtn: '.add__btn',
+        expenseList: '.expenses__list',
+        incomeList: '.income__list',
     }
 
     return {
@@ -21,18 +49,33 @@ const UIController = (() => {
                 description: document.querySelector(DOMStrings.description).value, //description
                 value: document.querySelector(DOMStrings.value).value, //value amount
             }
+        },
+
+        getDOMStrings: () => {
+            return DOMStrings;
         }
-    }
+    };
 
 })();
 
 //Global app controller
 const controller = ((budgetCtrl, UICtrl) => {
+    const eventListenerSetUp = () => {
+        const DOMStrings = UICtrl.getDOMStrings();
 
+        document.querySelector(DOMStrings.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', (e) => {
+            if(e.keyCode === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+    
     const ctrlAddItem = () => {
         //get the input data
-        let input = UICtrl.getInput();
-        console.log(input);
+        const input = UICtrl.getInput();
+        const DOMStrings = UICtrl.getDOMStrings();
 
         //add the item to the budget controller
         const listItem = `
@@ -49,9 +92,9 @@ const controller = ((budgetCtrl, UICtrl) => {
         `;
 
         if(input.type === 'exp') {
-            document.querySelector('.expenses__list').insertAdjacentHTML('beforeend', listItem);
+            document.querySelector(DOMStrings.expenseList).insertAdjacentHTML('beforeend', listItem);
         } else if(input.type === 'inc') {
-            document.querySelector('.income__list').insertAdjacentHTML('beforeend', listItem);
+            document.querySelector(DOMStrings.incomeList).insertAdjacentHTML('beforeend', listItem);
         }
         //add the item to the UI
 
@@ -59,14 +102,16 @@ const controller = ((budgetCtrl, UICtrl) => {
 
         //display the budget on the UI
         
-    }
-
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', (e) => {
-        if(e.keyCode === 13) {
-            ctrlAddItem();
-        }
-    });
+    };
     
+    return {
+        init: () => {
+            console.log('app has started');
+            eventListenerSetUp();
+        },
+    };
+
 })(budgetController, UIController);
+
+//initializes the event lisnters
+controller.init();
